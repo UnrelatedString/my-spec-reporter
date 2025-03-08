b3813348fcba54d1047351770c9e2feeca58854f
UnrelatedString <unrelatedstring@gmail.com>
2025-03-07 20:07:58 -0500

    wait so now it ohhh it only shows failures because of the defaultSummary huh

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import Effect (Effect)
 import Test.Spec(Spec, describe, it)
-import Test.Spec.Assertions (shouldEqual, shouldSatisfy, AnyShow(..))
+import Test.Spec.Assertions (shouldEqual, shouldSatisfy, fail, AnyShow(..))
 import Test.Spec.Runner.Node (runSpecAndExitProcess)
 
 import Test.Spec.Reporter.June.Pretty (prettyReporter)
@@ -14,3 +14,4 @@ main = runSpecAndExitProcess [prettyReporter] do
   describe ":3" do
     it "works" do
       1 `shouldEqual` 1
+      fail "it doesn't work :("
