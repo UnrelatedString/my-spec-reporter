bc32b67bd0f6a99cf235cf1957d0aead9f8fce0f
UnrelatedString <unrelatedstring@gmail.com>
2025-03-07 21:08:58 -0500

    so I guess it's like, the defaultUpdate stuff is just about re-sequencing parallel tests that actually ran out of order? hmm

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -3,9 +3,11 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Test.Spec(Spec, describe, it, pending)
+import Test.Spec(Spec, describe, it, pending, parallel)
 import Test.Spec.Assertions (shouldEqual, shouldNotEqual, shouldSatisfy, fail, AnyShow(..))
 import Test.Spec.Runner.Node (runSpecAndExitProcess)
+import Data.Time.Duration (Milliseconds(..))
+import Effect.Aff (delay)
 
 import Test.Spec.Reporter.June.Pretty (prettyReporter)
 
@@ -21,3 +23,14 @@ main = runSpecAndExitProcess [prettyReporter] do
       describe "too deep aaa" do
         it "uhh" do
           "this" `shouldNotEqual` "that"
+  parallel $ describe "parallel" do
+    it "1" do 1 `shouldEqual` 1
+    it "2" do 1 `shouldEqual` 1
+    it "3" do 1 `shouldEqual` 1
+    it "sloooow" do delay $ Milliseconds 1000.0
+    it "4" do 1 `shouldEqual` 1
+    it "5" do 1 `shouldEqual` 1
+    describe "A" do
+      it "super slow" do delay $ Milliseconds 5000.0
+    describe "B" do
+      it "fastish" do delay $ Milliseconds 50.0
