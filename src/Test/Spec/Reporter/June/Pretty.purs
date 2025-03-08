4353e0ae86cec311229c7e9338f1d51acaa0aa3d
UnrelatedString <unrelatedstring@gmail.com>
2025-03-08 15:31:49 -0500

    wahoo

--- src/Test/Spec/Reporter/June/Pretty.purs
+++ src/Test/Spec/Reporter/June/Pretty.purs
@@ -16,14 +16,14 @@ import Test.Spec.Speed as Speed
 import Test.Spec.Tree (Path, Tree, TestLocator, parentSuiteName)
 import Ansi.Codes (GraphicsParam(..))
 import Ansi.Codes as ANSI
-import Data.Tuple.Nested ((/\))
+import Data.Tuple.Nested (type (/\), (/\))
 import Data.Maybe (Maybe(..))
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Array as Array
 import Data.String as String
-import Control.Monad.State (class MonadState, StateT, get, modify)
-import Control.Monad.Writer (class MonadWriter, class MonadTell, Writer, tell, listen, censor, execWriterT)
+import Control.Monad.State (class MonadState, StateT, get, modify, put, execStateT)
+import Control.Monad.Writer (class MonadWriter, class MonadTell, Writer, tell, listen, censor, execWriter, runWriter)
 import Control.Monad.Trans.Class (lift)
 import Data.Unfoldable (replicate)
 import Data.Unfoldable.Trivial ((::<*>)) -- ...I sure hope this cyclical dependency isn't a problem if it's only for testing
@@ -31,6 +31,7 @@ import Data.Foldable (fold)
 import Data.List.NonEmpty (NonEmptyList(..))
 import Data.List ((:), List(Nil))
 import Data.NonEmpty (NonEmpty, (:|))
+import Data.Monoid.Additive (Additive(..))
 
 prettyReporter :: Reporter
 prettyReporter = defaultReporter initialState $ defaultUpdate
@@ -43,6 +44,7 @@ prettyReporter = defaultReporter initialState $ defaultUpdate
 type PrettyState = { runningItems :: Map TestLocator RunningItem, undoLastSequential :: Writer String Unit }
 
 type PrettyAction = StateT PrettyState (Writer String) Unit
+type UndoablePrint = StateT PrettyState (Writer (Additive Int /\ String)) Unit
 
 initialState :: PrettyState
 initialState = { runningItems: Map.empty, undoLastSequential: pure unit }
@@ -69,48 +71,64 @@ update (Event.Suite Sequential locator) = pure unit
 update (Event.Suite Parallel locator) = letDefaultUpdateHandleThis
 update (Event.SuiteEnd locator) = pure unit
 update (Event.Test Sequential locator) = do
-  indent locator
+  commit $ indent locator
   untell <- backspace $ formatTest locator Nothing
   void $ modify _{undoLastSequential = untell}
 update (Event.Test Parallel locator) = letDefaultUpdateHandleThis
 update (Event.TestEnd locator result) = do
   state <- get
   lift $ state.undoLastSequential
-  indent locator
-  formatTest locator $ Just result
+  commit $ formatTest locator $ Just result
   tellLn ""
 update (Event.Pending locator) = pure unit
 update (Event.End resultTrees) = defaultSummary resultTrees
 
-indent :: TestLocator -> PrettyAction
-indent (path /\ _) = tell $ fold ::<*> replicate (Array.length path) "| "
+indent :: TestLocator -> UndoablePrint
+indent (path /\ _) = (fold ::<*> replicate (Array.length path) "| ") `styled` (PForeground ANSI.White : Nil)
 
-styled :: String -> NonEmpty List GraphicsParam -> String
+styled :: String -> List GraphicsParam -> UndoablePrint
 -- fortunately the associativity of (<>) happens to be defined to make this legal LMAO
-styled s = (_ <> s <> clearFormatting) <<< ANSI.escapeCodeToString <<< ANSI.Graphics <<< NonEmptyList
+styled s (h : t) = tell $ (Additive (String.length s) /\ _) $
+                   (_ <> s <> clearFormatting) $
+                   ANSI.escapeCodeToString $ ANSI.Graphics $ NonEmptyList $ h :| t
+styled s Nil = tell $ Additive (String.length s) /\ s
 
 clearFormatting :: String
 clearFormatting = ANSI.escapeCodeToString $ ANSI.Graphics $ NonEmptyList $ ANSI.Reset :| Nil
 
-backspace :: forall m. MonadWriter String m => m Unit -> m (Writer String Unit)
-backspace action = do
-  _ /\ s <- listen action
-  pure $ tell $ fold ::<*> replicate (String.length s) "\x08"
+-- why do I feel like there's a way simpler canonical way to do this
+writeStatefullyOrSomething :: forall s w m. Monoid w => Monad m => StateT s (Writer w) Unit -> StateT s m w
+writeStatefullyOrSomething a = do
+  state <- get
+  let state' /\ output = runWriter $ execStateT a state
+  put state'
+  pure output
 
-formatTest :: TestLocator -> Maybe Result -> PrettyAction
+backspace :: UndoablePrint -> StateT PrettyState (Writer String) (Writer String Unit)
+backspace print = do
+  Additive n /\ s <- writeStatefullyOrSomething print
+  tell s
+  pure $ tell $ fold ::<*> replicate n "\x08"
+
+commit :: UndoablePrint -> PrettyAction
+commit print = do
+  _ /\ s <- writeStatefullyOrSomething print
+  tell s
+
+formatTest :: TestLocator -> Maybe Result -> UndoablePrint
 formatTest (_ /\ name) r = do
   formatTestResultIndicator r
-  tell $ name `styled` (PForeground ANSI.White :| Nil)
+  name `styled` (PForeground ANSI.White : Nil)
   formatTestResultSuffix r
 
-formatTestResultIndicator :: Maybe Result -> PrettyAction
-formatTestResultIndicator = const (tell " ") <=< tell <<< case _ of
-  Just (Success _ _) -> "✓" `styled` (PForeground ANSI.BrightGreen :| Nil)
-  Just (Failure _)   -> "✗" `styled` (PForeground ANSI.BrightRed :| PBackground ANSI.BrightWhite : Nil)
-  Nothing            -> "-" `styled` (PForeground ANSI.White :| PMode ANSI.Dim : Nil)
+formatTestResultIndicator :: Maybe Result -> UndoablePrint
+formatTestResultIndicator = const (" " `styled` Nil) <=< case _ of
+  Just (Success _ _) -> "✓" `styled` (PForeground ANSI.BrightGreen : Nil)
+  Just (Failure _)   -> "✗" `styled` (PForeground ANSI.BrightRed : PBackground ANSI.BrightWhite : Nil)
+  Nothing            -> "-" `styled` (PForeground ANSI.White : PMode ANSI.Dim : Nil)
 
-formatTestResultSuffix :: Maybe Result -> PrettyAction
-formatTestResultSuffix = const (tell " ") <=< tell <<< case _ of
-  Just (Success _ _) -> "... passed" `styled` (PForeground ANSI.White :| Nil)
-  Just (Failure _)   -> "... failed!" `styled` (PForeground ANSI.Red :| Nil)
-  Nothing            -> "..." `styled` (PForeground ANSI.White :| Nil)
+formatTestResultSuffix :: Maybe Result -> UndoablePrint
+formatTestResultSuffix = case _ of
+  Just (Success _ _) -> "... passed" `styled` (PForeground ANSI.White : Nil)
+  Just (Failure _)   -> "... failed!" `styled` (PForeground ANSI.Red : Nil)
+  Nothing            -> "..." `styled` (PForeground ANSI.White : Nil)
